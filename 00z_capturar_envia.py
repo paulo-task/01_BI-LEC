8ad6eb67c59004ceec5be11eb7c1943e5a4413f7
paulo-task <paulosantos.task@gmail.com>
2026-07-14 11:38:18 -0300

    up

--- 00z_capturar_envia.py
+++ 00z_capturar_envia.py
@@ -489,7 +489,7 @@ def capturar_powerbi():
 
             # Coordenadas exatas medidas pelo usuário na imagem Full HD (1920x1080)
             # X1, Y1, X2, Y2 = 260, 85, 1880, 1055
-            X1, Y1, X2, Y2 = 260, 135, 1790, 1045
+            X1, Y1, X2, Y2 = 260, 105, 1830, 1050
 
             # PAULISTA
             try:
